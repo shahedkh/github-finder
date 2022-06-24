33d3233cd410a246d8ebcfb8fe157b9b6ec2ff6a
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-24 04:42:24 +0300

    edit app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import { BrowserRouter as  Router, Route , Routes} from "react-router-dom";
-import Footer from "./components/layout/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Home from "./Pages/Home";
-import NotFound from "./Pages/NotFound";
-import About from "./Pages/About";
+import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import User from "./pages/User";
+import NotFound from "./pages/NotFound";
+import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
-import "./index.css";
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
 
-        <main className="container mx-auto px-3 h-screen">
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/*" element={<NotFound/>}/>
-          </Routes>
-        </main>
-        <Footer/>
-      </div>
-    </Router>
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
   );
 }
 
